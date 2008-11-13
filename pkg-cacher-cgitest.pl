047016f60fd73cb98796541011556314062910fc
Robert Nelson <robertn@the-nelsons.org>
2008-11-13 06:10:53 +0000

    Remove obsolete test script

--- pkg-cacher-cgitest.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-# pkg-cacher.pl - CGI to provide a local cache for debian packages and
-# release files and .deb files. Actually just a wrapper to set CGI mode flag
-# for the real script.
-
-$ENV{CGI_MODE}=1;
-
-# identify as CGI and run the actuall script
-require "/usr/src/pkg-cacher/src/pkg-cacher2";
